d9f9e2b56de0c7389ed28f49fe34fe0baa0f4811
nikita <nikitaniki1993@gmail.com>
2018-03-12 18:59:22 +0530

    hello world program reactjs

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <header className="App-header">
+          
+          <h1 className="App-title">Welcome to React</h1>
+          
+        </header> 
+        <p className="App-intro">
+          Hello World!!
+        </p>
+        <h2>Editing </h2>
+       
+        
+      </div>
+    );
+  }
+}
+
+export default App;
